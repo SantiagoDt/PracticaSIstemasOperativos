9edad1e3696f944d242861e8a003e89057991b6e
Isam El Mourabet <isamelmourabet@MacBook-Pro.local>
2024-03-12 15:58:57 +0100

    Cambio al controlar la salida al poner un comando no soportado

--- info_user.sh
+++ info_user.sh
@@ -42,7 +42,7 @@ elif test "$1" == "-g"
 then
   echo "Se debe proporcionar el nombre del grupo con el parámetro -g"
   else
-    echo "Error:Opción no valida.Use --help para obtener ayuda."
+    echo "Error: Opción no valida. Uso "\$0 [-u][-g]" Use --help para obtener ayuda."]
   fi
 
 elif test $# -eq 2
